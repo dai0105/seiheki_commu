a63b78a2feaa6a7cb93b263c502b9bf549d76d91
dai0105 <daiki4457@gmail.com>
2026-03-15 15:51:30 +0900

    Fix SECRET_KEY env loading

--- community/settings.py
+++ community/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_PRICE_ID = "price_1T5qvpPfF0XDfrqCnjS5kRf1"
