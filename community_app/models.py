04d9eb65953e8f0b7275adae92fb69704a4d0e72
dai0105 <daiki4457@gmail.com>
2026-03-16 13:58:22 +0900

    Add Profile auto-create signals and fix missing profile errors

--- community_app/models.py
+++ community_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import Tag
 from django.contrib.auth import get_user_model
+from .models import Profile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class FetishTag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -98,3 +101,14 @@ class Block(models.Model):
 
     def __str__(self):
         return f"{self.blocker} → {self.blocked}"
+    
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
