af49f7589c953a682322078095eb9fc740f47dcc
dai0105 <daiki4457@gmail.com>
2026-03-16 14:26:25 +0900

    Remove circular import from models.py

--- community_app/models.py
+++ community_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import Tag
 from django.contrib.auth import get_user_model
-from .models import Profile
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
